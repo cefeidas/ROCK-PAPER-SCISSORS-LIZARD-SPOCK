f9e7c694257f9e933b3890fd845466b21bd4b452
cefeidas <danmarmon1986@gmail.com>
2023-04-22 22:04:55 +0000

    add js to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,34 @@ document.addEventListener('DOMContentLoaded', function () {
         instructions.classList.add('hidden');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const startBtn = document.getElementById('start-btn');
+    const playerOptions = document.querySelectorAll('.player-options .custom-button');
+    const playerChoiceImages = document.querySelectorAll('.player-options img[id^="playerRoundOneChoice"]');
+    const villainChoiceImage = document.getElementById('villainRoundOneChoiceSpock');
+    const singleImageDiv = document.querySelector('.singleImage');
+    
+    startBtn.addEventListener('click', function() {
+        for (const button of playerOptions) {
+            button.addEventListener('click', function(event) {
+                // Mostrar el contenedor singleImage
+                singleImageDiv.classList.remove('hidden');
+
+                // Ocultar todas las imágenes de elección de jugador
+                for (const img of playerChoiceImages) {
+                    img.classList.add('hidden');
+                }
+                
+                // Mostrar la imagen correspondiente a la elección del jugador
+                const choiceImg = event.currentTarget.querySelector('img');
+                const choice = choiceImg.alt;
+                const playerChoiceImage = document.getElementById(`playerRoundOneChoice${choice}`);
+                playerChoiceImage.classList.remove('hidden');
+                
+                // Mostrar la imagen de elección del oponente (Spock)
+                villainChoiceImage.classList.remove('hidden');
+            });
+        }
+    });
+});
